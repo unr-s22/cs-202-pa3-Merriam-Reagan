35f3402b3a8556b6342b7fd05e34ce8d5e12459e
Vincent Z Bongarzone <vbongarzone@nxlogin.rd.unr.edu>
2022-03-30 23:08:02 -0700

    message

--- account.h
+++ account.h
@@ -3,8 +3,22 @@
 #include "money.h"
 #include <vector>
 
+using namespace std;
+
 class Account{
 
+	Money Money;
+	vector<Money> depVec;
+	vector<Money> withVec;
+	bool needUpdate = false;
+	
+public:
+	Account(Money);
+	void deposit(Money);
+	void withdrawal(Money);
+	Money balance();
+	friend ostream& opreator << (ostream&, Account& account);
+	
 
 };
 #endif
--- main.cpp
+++ main.cpp
@@ -1,8 +1,33 @@
-#include"money.h"
-#include "account.h"
+#include"Money.h"
+#include "Account.h"
 
+using namespace std;
 
 int main(){
+	
+	Money Money(300, 23)
+	Account account(Money)
+	cout << account << endl;
+	account.deposit(Money(200,00));
+	account.deposit(Money(300,24));
+	account.deposit(Money(501,22));
+	cout << account << endl;
+	account.withdrawal(Money(300,10));
+	account.withdrawal(Money(201,34));
+	cout << account << endl;
+
+	cout << "=================" endl;
+	cout << "Relations between " << Money << " and " << account.balance() << endl;
+	cout << "--------------------" << endl;
+	
+
+	//verifies that relational operators are working
+	cout << "Is " << Money << " > " << account.balance() << ": " << (Money > account.balance()) << endl;
+	cout << "Is " << Money << " < " << account.balance() << ": " << (Money > account.balance()) << endl;
+	cout << "Is " << Money << " >= " << account.balance() << ": " << (Money > account.balance()) << endl;
+	cout << "Is " << Money << " <= " << account.balance() << ": " << (Money > account.balance()) << endl;
+	cout << "Is " << Money << " == " << account.balance() << ": " << (Money > account.balance()) << endl;
+	cout << "Is " << Money << " != " << account.balance() << ": " << (Money > account.balance()) << endl;
 
 	return 0;
 }
